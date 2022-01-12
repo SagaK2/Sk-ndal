51a7355988e5564d57485a5bd35d286bcde90ca0
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2022-01-12 18:40:57 +0100

    Påbörjan av klick/raycast till playern

---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public Interface ITakeable
+{
+    void Take(Take take)
+
+    //get och set
+}*/
---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour//, ITakeable
+{
+    public Camera camera;
+
+    RaycastHit hit;
+
+    void Start()
+    {
+        camera.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            //Om objektet träffar något. Vad ska den göra
+            print("hit");
+            //ITakeable take = hit.transform.GetComponent(typeof(ITakeable)) as ITakeable;
+            string tag = hit.collider.gameObject.tag;
+        }
+    }
+}
