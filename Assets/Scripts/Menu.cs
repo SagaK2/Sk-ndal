4698820460efb65867ff34d70eb8f5f3b0df48cb
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2022-01-13 09:45:39 +0100

    Start av menu och snart klar med att kunna dra ett föremål

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    //Kommer använda funktionerna(?) med knapparna i Unity - Saga 
+
+    public GameObject optionMenu;
+    public /*virtual jag vet inte om vi vill ha en pausmeny än, annars kan den scripten ära från denna*/ void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public /*virtual*/ void EndGame()
+    {
+        Application.Quit();
+    }
+}
