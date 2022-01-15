957859460fd83ac24965825726ebdcc7b5392916
JonathanLmao <79579327+JonathanLmao@users.noreply.github.com>
2022-01-15 21:12:42 +0100

    Rörelse
    Du kan nu ta en promenad runt gullmarsplan, Kolla bara inte in i några speglar.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,10 +6,14 @@ public class Movement : MonoBehaviour
 {
     //En variabel som gör mus sensitivity till en siffra vi kan ändra i inspektorn. JR
     public float Sensitivity = 100f;
-    //En variabel för vårans spelares transform, alltså position, rotation och skala. JR
-    public Transform playerModel;
-    //
+    //En variabel för våran spelares transform, alltså position, rotation och skala. JR
+    public Transform spelare;
+    //x-rotation vet inte riktigt vad den gör men behövs. JR
     float Xpos = 0f;
+    //referens till spelaren
+    public CharacterController Player;
+    //Låter oss välja fart
+    public float speed = 12;
 
     //Vi måste göra så att man kan använda musen till att spela spelet - Saga
     bool miniGame;
@@ -23,16 +27,26 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Gör om musens position till en float som kan sparas. JR
+        //referens till mouse x och mouse y från unity inputmanager. deltaTime gör så rotationen är oberoende av FPS. JR
         float mouseX = Input.GetAxis("Mouse X") * Sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * Sensitivity * Time.deltaTime;
         //Varje frame minskar x-rotationen baserat på mouse Y. JR
         Xpos -= mouseY;
-        //Gör så man inte kan bryta nacken genom att kolla bakom sig på Y-axeln. JR
+        //Gör så man inte kan bryta nacken när man kollar bakom sig på Y-axeln. JR
         Xpos = Mathf.Clamp(Xpos, -90f, 90f);
-        //
+        //låter dig rotera (kolla runt in-game). JR
         transform.localRotation = Quaternion.Euler(Xpos, -182, 0f);
         //Rotera runt spelarens Y-axel när man rör runt musen på X-axeln. JR
-        playerModel.Rotate(Vector3.up * mouseX);
+        spelare.Rotate(Vector3.up * mouseX);
+
+        //Koden Nedan är för att faktiskt röra sig. Borde fungera med kontroll också. JR
+
+        //referenser till inputmanager igen. JR
+        float Zmovement = Input.GetAxis("Vertical");
+        float xmovement = Input.GetAxis("Horizontal");
+        //Väljer en riktning som är i förhållande till spelaren (alltså vänster eller höger istället för väst eller öst). JR
+        Vector3 rörelse = transform.right * xmovement + transform.forward * Zmovement;
+        //Faktiskt rör oss åt den riktningen. Detta göra oss också inte snabbare ifall vi har högre FPS JR
+        Player.Move(rörelse*speed*Time.deltaTime);
     }
 }
