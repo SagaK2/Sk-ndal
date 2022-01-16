70c47ea3ea80cf62c57edbbb35375c6bf4d92c64
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2022-01-16 20:35:39 +0100

    Fixar animationer till Ghoulmar
    Behövde dock sluta för ska gå ut med Ludvig

--- Assets/Scripts/GhoulmarMovement.cs
+++ Assets/Scripts/GhoulmarMovement.cs
@@ -6,14 +6,19 @@ public class GhoulmarMovement : MonoBehaviour
 {
     //Sagas kod
     public Rigidbody rb;
+    public Animator animator;
+
+    bool walking;
+    bool running;
     void Start()
     {
         
+
     }
 
     
     void Update()
     {
-        rb.velocity = new Vector3(-2, 0, 0);
+        rb.velocity = new Vector3(0, 0, 2);
     }
 }
