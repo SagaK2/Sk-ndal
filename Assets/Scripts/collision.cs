f30c063d98b97c9b5dfd0cf37325ab9f3eba49ee
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2022-01-20 19:16:38 +0100

    Text till skåp

--- Assets/Scripts/collision.cs
+++ Assets/Scripts/collision.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class collision : MonoBehaviour
 {
+    //Sagas kod
+    public GameObject text;
+
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-
         if (hit.gameObject.CompareTag("Ghoulmar"))
         {
-            SceneManager.LoadScene(6);
+            //Då när Ghoulmar rört vid än, då ska en annan scen med en jumpscare dyka upp
+            SceneManager.LoadScene(4);
+
+        }else if (hit.gameObject.CompareTag("ToClose"))
+        {
+            //Man är för nära skåpet när man nuddar denna collider. Därför poppar det upp en text som säger att an inte borde vara det
+            text.SetActive(true);
+        }
+        else
+        {
+            //Man ska annars inte se texten eller när man är för nära/nuddar vid collidern
+            text.SetActive(false);
         }
     }
 }
