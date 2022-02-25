1ac2b1a1b0c489018197bb858a04999d85540361
Emrezz145 <emre.uludag@elev.ga.lbs.se>
2022-02-25 13:36:31 +0100

    buttonnoise

---
+++ Assets/Scripts/buttonclicknoise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonclicknoise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource buttonnoise;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            buttonnoise.Play();
+        }
+    }
+}
