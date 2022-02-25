9b92cf5e6bf1308a35716118c0fae5ff9deb0b2a
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2022-02-25 13:36:00 +0100

    fdjhjhksdds

--- Assets/Scripts/PlayerTrigger.cs
+++ Assets/Scripts/PlayerTrigger.cs
@@ -21,47 +21,49 @@ public class PlayerTrigger : MonoBehaviour
 
     void Start()
     {
+        //För att man ska kunna transporteras till samma position man startar på gör jag en vector som är vart spelaren är i start
         startPos = transform.position;
+        //Man ska inte kunna gå igneom collider väggar runt banan. Men man 
         returnHome = false;
-    }
-
-    public void Return()
-    {
-        returnHome = true;
     }
 
     void Update()
     {
         if (transform.position == startPos)
         {
+            //Om man transporterats tillbaka till start ska man inte hela tiden fortsätta åka tillbaka för att man kör
             returnHome = false;
         }
         else if(returnHome)
         {
+            //Men om man nuddat en vägg och returnHome blir sann då ska man transporteras till start positionen som sparades i början
             transform.position = startPos;
         }
     }
 
     void OnTriggerEnter(Collider collider)
     {
+        //Om spelaren nuddar väggen i minispelet.. 
         if (collider.gameObject.CompareTag("Wall"))
         {
-            Return();
-            //Kan annars bara skriva den raden kod?
+            //Kommer den transporteras hem igen
+            returnHome = true;
         }
         else if (collider.gameObject.CompareTag("Finish"))
         {
+            //Men om man nuddar collider väggen vid målet.. 
             clickDrag.Start();
             //Då sätts isDragging till false eftersom den annars sätts till false
             movement.Start();
-            movement.PlayerMovement();
             //Som sätter curser lock till locked mode
+            movement.PlayerMovement();
+            //Man kommer kunna röra sig igen som vanligt
             ghoulmar.SetActive(true);
             //Ghoulmar sätts på
             miniGame.SetActive(false);
-            //röd FF0000
+            //minigamet försvinner i syne
             lampLight.color = lampColor;
-            //Lampan ska lysa grönt, animation
+            //Färgen som annars lyser rött blir till färgen man valt i unity som just nu är grön
             //SceneManager.LoadScene(3); Skickade spelaren till end scene men man måste nu ha tre ellådor inte bara en
 
         }
